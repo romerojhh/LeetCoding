c46ca45bfef822b634c71dc4e1ad8d889ca8dd1d
MrSaladdd <61641738+MrSaladdd@users.noreply.github.com>
2022-03-05 13:13:10 -0800

    finished # 695. Max Area of Island

--- src/main/MaxAreaOfIsland.java
+++ src/main/MaxAreaOfIsland.java
@@ -1,6 +1,6 @@
 package main;
 
-/* TODO: WORK ON THIS CLASS
+/*  WORK ON THIS CLASS
     695. Max Area of Island
 
     You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally
@@ -11,29 +11,45 @@ package main;
     Return the maximum area of an island in grid. If there is no island, return 0.
  */
 
-public class TODOMaxAreaOfIsland {
+public class MaxAreaOfIsland {
     public static void main(String[] args) {
-        TODOMaxAreaOfIsland m = new TODOMaxAreaOfIsland();
-        m.maxAreaOfIsland(new int[][]{{1,0,1,1}, {1,0,1,1}, {0,0,0,0}, {0,0,0,0}});
+        MaxAreaOfIsland m = new MaxAreaOfIsland();
+        // System.out.println(m.maxAreaOfIsland(new int[][]{{1,0,1,1}, {1,0,1,1}, {0,0,0,0}, {0,0,0,0}}));
+        System.out.println(m.maxAreaOfIsland(new int[][]{{0}, {1}}));
     }
 
     public int maxAreaOfIsland(int[][] grid) {
+        int sum = 0;
         for (int row = 0 ; row < grid.length ; row++) {
-            for (int col = row; col < grid[row].length ; col++) {
+            for (int col = 0; col < grid[row].length ; col++) {
                 if (grid[row][col] == 1) {
-                    helper(row, col, 0, grid);
+                    sum = Math.max(sum, helper(row, col, 1, grid));
                 }
             }
         }
-        return -1;
+        return sum;
     }
 
-    private void helper (int row, int col, int sum, int[][] grid) {
+    private int helper (int row, int col, int sum, int[][] grid) {
 
         if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length || grid[row][col] != 1) {
-            return;
+            return 0;
         }
+        grid[row][col] = 0;
 
+        // only add the sum if it's valid
+        // sum += 1;
+        int recurseUp = helper (row - 1, col, sum, grid);
+        int recurseDown = helper (row + 1, col, sum, grid);
+        int recurseLeft = helper (row, col - 1, sum, grid);
+        int recurseRight = helper (row, col + 1, sum, grid);
+
+        return recurseDown + recurseUp + recurseLeft + recurseRight + sum;
+
+        /*
+        return helper (row - 1, col, sum + 1, grid) + helper (row + 1, col, sum + 1, grid) +
+                helper (row, col - 1, sum + 1, grid) + helper (row, col + 1, sum + 1, grid);
+        /*
         //if (row >= 0 && row < grid.length && col >= 0 && col < grid[row].length && grid[row][col] == 1) {
             // recurse up
             helper (row - 1, col, sum + 1, grid);
@@ -44,5 +60,7 @@ public class TODOMaxAreaOfIsland {
             // recurse right
             helper (row, col + 1, sum + 1, grid);
         //}
+
+         */
     }
 }
