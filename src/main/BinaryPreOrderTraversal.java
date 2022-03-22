bfd518bac16c66876561d90b29764d2a910ae86b
MrSaladdd <61641738+MrSaladdd@users.noreply.github.com>
2022-03-21 19:05:02 -0700

    modif

--- src/main/BinaryPreOrderTraversal.java
+++ src/main/BinaryPreOrderTraversal.java
@@ -1,31 +1,31 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
 public class BinaryPreOrderTraversal {
-    public static void main (String[] args) {
-        TreeNode root = new TreeNode(7, new TreeNode(5, new TreeNode(4), new TreeNode(6)),
-                new TreeNode(9, new TreeNode(8), new TreeNode(10)));
-        BinaryPreOrderTraversal bp = new BinaryPreOrderTraversal();
-        bp.preorderTraversal(root);
-    }
     public List<Integer> preorderTraversal(TreeNode root) {
+        // Go to the leftmost leaf first
         Stack<TreeNode> stack = new Stack<>();
         List<Integer> list = new ArrayList<>();
-        TreeNode currNode = root;
 
-        while(currNode != null || !stack.empty()) {
+        stack.push(root);
+        list.add(root.val);
+        TreeNode currNode = root.left;
+
+        while (!stack.empty()) {
             while (currNode != null) {
                 stack.push(currNode);
                 list.add(currNode.val);
-                currNode = currNode.right;
+                currNode = currNode.left;
             }
-            currNode = stack.pop();
-            currNode = currNode.left;
+            // we reach the leftMostNode here!
+            TreeNode leftMost = stack.pop();
+
+            currNode = stack.peek().right;
         }
+
         return list;
     }
 }
